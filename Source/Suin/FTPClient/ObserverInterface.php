24e7e9af66c2901d6c846ae82aa953cd0477d03c
Guilherme Nascimento <brcontainer@yahoo.com.br>
2015-12-03 10:18:26 -0200

    Update ObserverInterface.php

--- Source/Suin/FTPClient/ObserverInterface.php
+++ Source/Suin/FTPClient/ObserverInterface.php
@@ -2,18 +2,18 @@
 
 interface Suin_FTPClient_ObserverInterface
 {
-	/**
-	 * @abstract
-	 * @param string $request
-	 * @return void
-	 */
-	public function updateWithRequest($request);
+    /**
+     * @abstract
+     * @param string $request
+     * @return void
+     */
+    public function updateWithRequest($request);
 
-	/**
-	 * @abstract
-	 * @param string $message
-	 * @param int $code
-	 * @return void
-	 */
-	public function updateWithResponse($message, $code);
+    /**
+     * @abstract
+     * @param string $message
+     * @param int $code
+     * @return void
+     */
+    public function updateWithResponse($message, $code);
 }
